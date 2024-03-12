7c0525776910fd6a3d1d71a8fe460eba0b75874a
DESKTOP-F12VINP\User <yush@obzen.com>
2024-03-12 18:36:30 +0900

    2024.03.12 프록시

---
+++ src/main/java/com/study/jpastudy/chapter8/ex8_1/Member8_1.java
@@ -0,0 +1,17 @@
+package com.study.jpastudy.chapter8.ex8_1;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Member8_1 {
+    private String username;
+
+    @ManyToOne
+    private Team8_1 team;
+
+}
---
+++ src/main/java/com/study/jpastudy/chapter8/ex8_1/Team8_1.java
@@ -0,0 +1,12 @@
+package com.study.jpastudy.chapter8.ex8_1;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Team8_1 {
+    private String name;
+}
